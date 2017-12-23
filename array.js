356002c70e7bf184b747064c0e92d22a34812bf5
krishna1029 <34799381+krishna1029@users.noreply.github.com>
2017-12-23 14:35:10 -0600

    Added Js and html files

---
+++ array.js
@@ -0,0 +1,14 @@
+var a=[];
+var b= new Array();
+console.log(a);
+console.log(b);
+var c= [55,true,'shgfgd',undefined,null,function(a,b){return a*b; },{}];
+console.log(c);
+var d= new Array(1,2,3,4,5);
+console.log(d);
+var h=new Array(5);
+console.log(h);
+var e= new Array(55,true,'shgfgd',undefined,null,function(a,b){return a*b; },{});
+console.log(e);
+console.log(e.length);
+
